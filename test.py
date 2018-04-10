069858c10d0b839aae4fb64bf26949165c7f2fc4
Oleg Sokolov <olegsokolov91@gmail.com>
2018-04-10 11:57:11 +0200

    add Test map func

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ import sys
 
 from pyactor.context import set_context, create_host, sleep, shutdown
 
+from Timer import Timer
 from MapReduce import Mapper, Reducer
 from Registry import Registry
 
@@ -29,8 +30,11 @@ class BasicTest(unittest.TestCase):
         self.registry = self.h.spawn('Registry', Registry)
         self.reducer = self.h.spawn('Reducer', Reducer)
         self.mapper = self.h.spawn('Mapper', Mapper)
+        self.timer = self.h.spawn('Timer', Timer)
         self.registry.bind('Reducer', self.reducer)
         self.registry.bind('Mapper', self.mapper)
+        self.url_server = '127.0.0.1'
+        self.url_file = 'http://' + self.url_server + '/pg2000.txt'
         self.stdo = sys.stdout
         self.out = Outs()
         sys.stdout = self.out
@@ -42,16 +46,12 @@ class BasicTest(unittest.TestCase):
         self.out.clear()
         sys.stdout = self.stdo
 
-    def test_unbind(self):
-        """
-        Test the bind and unbind in the registry.
-        :return:
-        """
-        self.registry.unbind('Reducer')
-        self.registry.unbind('Mapper')
-        self.assertRaises(KeyError, self.registry.lookup, 'Reducer')
-        self.assertEqual(self.registry.lookup('Mapper'), None)
-        self.assertListEqual(self.registry.get_all(), [])
+    def test_map(self):
+        # This is the test. You can put as much of them as you want. The name
+        # must begin with 'test'.
+        self.mapper.start_map(self.url_file, self.reducer, self.timer)
+        sleep(.5)
+        self.assertEquals("Bot : ...\n", self.out.lines)
 
 
 if __name__ == '__main__':
